aa76b5457e7d29894f097adfed48bcdc8cb8733d
Pavel <studentikua@gmail.com>
2022-08-18 12:42:12 +0300

    init

---
+++ src/components/Title.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="title">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Title',
+  props: {
+    title: String
+  }
+}
+</script>
+
+
+<style scoped>
+.title {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+
+</style>
